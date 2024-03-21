2d91e0ea035003f4faf90a6548f7363a4aba0a70
liuzhaoming <zhaoming.liu@smartmore.com>
2024-03-21 10:54:49 +0800

    修改label生成规则

--- dataset/cls_dataset.py
+++ dataset/cls_dataset.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 
+
 class ImageInfo:
     def __init__(self, row):
         self._data = row
@@ -16,38 +17,34 @@ class ImageInfo:
 
     @property
     def label(self):
-        try:
-            return int(self._data[1])
-        except ValueError:
-            return self._data[1]
+        return int(self._data[1])
 
 
-
-class  ClsDataset(Dataset):
+class ClsDataset(Dataset):
     def __init__(self, list_file, transform=None):
         self.list_file = list_file
         self.transform = transform
-        
+
         self._parser_input_data()
-                 
+
     def _parser_input_data(self):
         assert os.path.exists(self.list_file)
-        
-        lines = [x.strip().split('\t') for x in open(self.list_file, encoding='utf-8')]
-        
-        self.imgs_list = [ImageInfo(line) for line in lines]
 
+        lines = [x.strip().split('\t') for x in open(self.list_file, encoding='utf-8')]
+
+        self.imgs_list = [ImageInfo(line) for line in lines]
 
     def __getitem__(self, index):
         img_info = self.imgs_list[index]
-    
+
         img = Image.open(img_info.path).convert('RGB')
         if self.transform != None:
-            img =self.transform(img)
+            img = self.transform(img)
         return img, torch.as_tensor(img_info.label), img_info.path
- 
+
     def __len__(self):
         return len(self.imgs_list)
+
 
 if __name__ == '__main__':
     clsdataset = ClsDataset(list_file='list.txt')
--- tools/label.py
+++ tools/label.py
@@ -1,4 +1,5 @@
 import os
+import string
 
 from utils import parser
 
@@ -21,15 +22,23 @@ def generate_labels(dataset_path):
                 print("done")
 
 
+# 生成label文件，格式为 图片路径  标签（数字） 目录名（）
+# 对于目录要求1_cat格式,前面数字作为训练label,因为label要求数字，要是自动生成这个label,那么后续每次增加目录可能导致混乱
 def generate_label_file(path, label_file_name):
     with open(label_file_name, 'w', encoding='utf8') as f:
-        for label_name in os.listdir(path):
-            d_path = os.path.join(path, label_name)
+        for d in os.listdir(path):
+            labels = d.split("_", maxsplit=1)
+            if len(labels) < 2 or (not labels[0].isdigit()):
+                raise ValueError(f"Invalid directory name:{d}, must be in the format of <number>_dirname")
+
+            label = labels[0]
+
+            d_path = os.path.join(path, d)
             if os.path.isdir(d_path):
                 for file in os.listdir(d_path):
                     file_path = os.path.join(d_path, file)
                     if os.path.isfile(file_path):
-                        f.write(f"{file_path}\t{label_name}\n")
+                        f.write(f"{file_path}\t{label}\t{labels[1]}\n")
 
 
 if __name__ == "__main__":
